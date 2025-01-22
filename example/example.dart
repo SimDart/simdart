5afb404701045f811e2b8963980351cfcdf3b5d4
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2025-01-22 08:32:51 -0300

    Adding a simple example.

--- example/example.dart
+++ example/example.dart
@@ -3,22 +3,21 @@ import 'package:simdart/simdart.dart';
 void main() async {
   final SimDart sim = SimDart(onTrack: (track) => print(track));
 
-  //sim.addEventScheduler(EventScheduler(start: 0, interval: Interval.fixed(fixedInterval: 1, untilTime: 3), event: eventA, name: 'S', eventName: 'A'));
-  //sim.process(eventA, name: 'A');
-  //sim.process(eventB, start: 5, name: 'B');
+  sim.process(event: _a, name: 'A');
+  sim.process(event: _b, start: 5, name: 'B');
 
   await sim.run();
 }
 
-void eventA(EventContext context) async {
-  // await context.wait(10);
-  // context.sim.process(eventC, delay: 1, name: 'C');
+void _a(EventContext context) async {
+  await context.wait(10);
+  context.sim.process(event: _c, delay: 1, name: 'C');
 }
 
-void eventB(EventContext context) async {
+void _b(EventContext context) async {
   await context.wait(1);
 }
 
-void eventC(EventContext context) async {
+void _c(EventContext context) async {
   await context.wait(10);
 }
